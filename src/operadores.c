9c2dce0d1969896b283eb466e6ba1e99edff9e13
rexim <reximkut@gmail.com>
2021-05-30 11:34:45 -0300

    demas funciones

--- src/operadores.c
+++ src/operadores.c
@@ -1,13 +1,28 @@
 #include "operadores.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int suma(int *args) {
   return args[0] + args[1];
 }
 
-int main() {
-    int A[2]={3, 4};
-    printf("%i", suma(A));
-  return 0;
+int resta(int *args) {
+    return args[0] - args[1];
+}
+
+int opuesto(int *args) {
+    return (-1) * args[0];
+}
+
+int division(int *args) {
+    return round(args[0] / args[1]);
+}
+
+int modulo(int *args) {
+    return args[0] % args[1];
+}
+
+int potencia(int *args) {
+    return pow(args[0], args[1]);
 }
