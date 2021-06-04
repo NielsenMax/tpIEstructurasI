aefcc7c9764931bc0c69ad555f3584f2441a6167
AlejoGonzalez <alejogonzalezips@gmail.com>
2021-06-04 18:34:25 -0300

    makefilev2

--- src/interprete.c
+++ src/interprete.c
@@ -250,24 +250,3 @@ void presentacion() {
 
 }
 
-int main() {
-  TablaOps tabla = NULL;
-  cargar_operador(&tabla, "+", 2, suma);
-  cargar_operador(&tabla, "-", 2, resta);
-  cargar_operador(&tabla, "--", 1, opuesto);
-  cargar_operador(&tabla, "*", 2, producto);
-  cargar_operador(&tabla, "/", 2, division);
-  cargar_operador(&tabla, "%", 2, modulo);
-  cargar_operador(&tabla, "^", 2, potencia);
-
-  ATree T = NULL;
-
-  presentacion();
-  interpretar(T, tabla);
-
-
-  liberar_tabla(tabla);
-
-
-
-}
